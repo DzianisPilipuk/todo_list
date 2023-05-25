0fe8c1d3315f8439bf6b435f28ff3da021500c7f
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-05-25 20:24:03 +0200

    Add create new task button function

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("test");
+(()=>{const e=document.getElementById("create_task_button"),t=document.getElementById("task_form_screen");document.getElementById("add_task_button").addEventListener("click",(()=>{t.style.visibility="hidden"})),e.addEventListener("click",(()=>{t.style.visibility="visible"}))})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("test");
+const createTaskButton = document.getElementById("create_task_button");
+const taskFormScreen = document.getElementById("task_form_screen");
+const taskAddingButton = document.getElementById("add_task_button");
+
+const showTaskFormScreen = () => {
+  taskFormScreen.style.visibility = "visible";
+};
+
+const hideTaskFormScreen = () => {
+  taskFormScreen.style.visibility = "hidden";
+};
+
+taskAddingButton.addEventListener("click", () => {
+  hideTaskFormScreen();
+});
+
+createTaskButton.addEventListener("click", () => {
+  showTaskFormScreen();
+});
